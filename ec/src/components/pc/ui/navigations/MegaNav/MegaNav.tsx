8c9589f1d8185d5b07adddfc709f8aa193cd20a1
**** <*****>
2023-12-20 15:59:05 +0900

    economy icon attach in meganav

--- ec/src/components/pc/ui/navigations/MegaNav/MegaNav.tsx
+++ ec/src/components/pc/ui/navigations/MegaNav/MegaNav.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './MegaNav.module.scss';
 import { useTopCategories } from '@/components/pc/ui/navigations/MegaNav/MegaNav.hooks';
 import { SkeletonTopCategoryItem } from '@/components/pc/ui/navigations/MegaNav/SkeletonTopCategoryItem';
 import { TopCategoryItem } from '@/components/pc/ui/navigations/MegaNav/TopCategoryItem';
 import { range } from '@/utils/number';
+import { Link } from './Link';
+import classNames from 'classnames';
 
 type Props = {
 	/** click category link handler */
@@ -19,9 +21,30 @@ export const MegaNav: React.VFC<Props> = ({ onClickLink }) => {
 
 	const categoryList = useTopCategories();
 
+	const [isHover, setIsHover] = useState(false);
+
+	const handleMouseEnter = () => {
+		setIsHover(true);
+	};
+	const handleMouseLeave = () => {
+		setIsHover(false);
+	};
 	return (
 		<div className={styles.meganavCategory}>
 			<ul>
+				<li
+					className={
+						isHover
+							? classNames(styles.miconT, styles.on)
+							: classNames(styles.miconT)
+					}
+					onMouseEnter={handleMouseEnter}
+					onMouseLeave={handleMouseLeave}
+				>
+					<Link href={``} onClick={onClickLink}>
+						경제형 제품
+					</Link>
+				</li>
 				{categoryList
 					? categoryList.map(category => (
 							<TopCategoryItem
